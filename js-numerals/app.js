5983290c0f6b32e683196893fdd434df8f3e1741
Horváth Donát <hordon@hordon.local>
2019-12-15 10:55:30 +0100

    handle edge cases

--- js-numerals/app.js
+++ js-numerals/app.js
@@ -1,5 +1,16 @@
+"use strict";
+
 const convert = function (num) {
-  return false
+
+  if (isNaN(num)) {
+    throw new TypeError('❗️You can only use numbers.');
+  } else if (!Number.isInteger(num)) {
+    throw new TypeError('❗️Please use integers only.')
+  } else if (num.toString().length > 10) {
+    throw new RangeError('Please use a number with less than 10 digits.')
+  }
+
+  return '';
 };
 
 module.exports = convert;
--- js-numerals/test/test.js
+++ js-numerals/test/test.js
@@ -3,39 +3,47 @@ const convert = require('../app');
 
 describe('AppTest', function () {
 
-  it('should throw error', function () {
+  it('string input should throw type error', function () {
     expect(() => convert('number')).to.throw('❗️You can only use numbers.');
   });
 
-  it('should return with a string', function () {
+  it('float input should throw type error', function () {
+    expect(() => convert(1.2)).to.throw('❗️Please use integers only.');
+  });
+
+  it('long number should throw overflow error', function () {
+    expect(() => convert(12345678910)).to.throw('Please use a number with less than 10 digits.');
+  });
+
+  it('fn should return with a string', function () {
     expect(convert(7)).to.be.a('string');
   });
 
-  it('should return zero', function () {
+  it('fn should return zero', function () {
     expect(convert(0)).to.equal('zero');
   });
 
-  it('should return seven', function () {
+  it('fn should return seven', function () {
     expect(convert(7)).to.equal('seven');
   });
 
-  it('should return forty-two', function () {
+  it('fn should return forty-two', function () {
     expect(convert(42)).to.equal('forty-two');
   });
 
-  it('should return two thousand and one', function () {
+  it('fn should return two thousand and one', function () {
     expect(convert(2001)).to.equal('two thousand and one');
   });
 
-  it('should return nineteen hundred and ninety-nine', function () {
+  it('fn should return nineteen hundred and ninety-nine', function () {
     expect(convert(1999)).to.equal('nineteen hundred and ninety-nine');
   });
 
-  it('should return seventeen thousand nine hundred and ninety-nine', function () {
+  it('fn should return seventeen thousand nine hundred and ninety-nine', function () {
     expect(convert(17999)).to.equal('seventeen thousand nine hundred and ninety-nine');
   });
 
-  it('should return minus eleven', function () {
+  it('fn should return minus eleven', function () {
     expect(convert(-11)).to.equal('minus eleven');
   });
 
